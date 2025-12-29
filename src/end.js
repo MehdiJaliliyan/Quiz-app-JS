327e5510f6f87a69564aa84e577214f8db97678e
MehdiJalilian <mhdijaliliyan@gmail.com>
2025-12-29 14:46:04 +0330

    show score on main page

--- src/end.js
+++ src/end.js
@@ -1,10 +1,13 @@
 const playAgainBtn = document.querySelector("#play_again");
 const homeBtn = document.querySelector("#home");
+const finalScore = document.querySelector('.score')
 
 playAgainBtn.addEventListener("click", () => {
   window.location.assign("../index.html");
 });
 
 homeBtn.addEventListener("click", () => {
-  window.localStorage.assign("start.html");
+  window.location.assign("start.html");
 });
+
+finalScore.innerText = `Final Score: ${localStorage.getItem('finalScore')}`
