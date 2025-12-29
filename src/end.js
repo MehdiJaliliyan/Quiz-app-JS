89479d378c50fb5c99368aeea5249f7787813037
MehdiJalilian <mhdijaliliyan@gmail.com>
2025-12-29 15:57:16 +0330

    change files location

--- src/end.js
+++ src/end.js
@@ -3,11 +3,11 @@ const homeBtn = document.querySelector("#home");
 const finalScore = document.querySelector('.score')
 
 playAgainBtn.addEventListener("click", () => {
-  window.location.assign("../index.html");
+  window.location.assign("main.html");
 });
 
 homeBtn.addEventListener("click", () => {
-  window.location.assign("start.html");
+  window.location.assign("index.html");
 });
 
 finalScore.innerText = `Final Score: ${localStorage.getItem('finalScore')}`
--- src/start.js
+++
@@ -1,6 +0,0 @@
-const startGameBtn = document.querySelector('.btn')
-console.log(startGameBtn);
-
-startGameBtn.addEventListener('click',() => {
-  window.location.assign('../index.html')
-})
